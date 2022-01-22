c92dd72d4444833c37a8e0a8d1fc1db24f835484
Karthik Raju <karthikraju391@gmail.com>
2022-01-22 13:25:08 +0530

    fix server error

--- index.js
+++ index.js
@@ -13,9 +13,6 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json()); // allows us to access the req.body
 
-// app.use(express.static(path.join(__dirname, 'client/build')));
-app.use(express.static("./client/build"))
-
 if (process.env.NODE_ENV === 'production') {
    // serve static content
    // npm run build
